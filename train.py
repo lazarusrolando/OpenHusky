7566ab2042835b0cce44ffd9cf8a448d13fa4045
lazarusrolando <72651696+lazarusrolando@users.noreply.github.com>
2026-05-25 13:45:50 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-7B-Instruct", trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-7B-Instruct", trust_remote_code=True)
+#Note: model = tokenizer at the double quotes
+model = AutoModelForCausalLM.from_pretrained("", trust_remote_code=True) #Any model from HuggingFace
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-7B-Instruct", trust_remote_code=True) #Any model from HuggingFace
 
 tokenizer.pad_token = tokenizer.eos_token
 
-model.save_pretrained("./openhusky-7B-instruct",safe_serialization=False)
-tokenizer.save_pretrained("./openhusky-7B-instruct")
+model.save_pretrained("<your-model-folder-name>",safe_serialization=False)
+tokenizer.save_pretrained("<your-model-folder-name>")
